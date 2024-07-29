9246e89e0896d73b3abcea5184d4a0ab9937f234
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2024-07-29 22:03:07 +0900

    Revert "表示を細かくする試し"
    This reverts commit 266b4c3196e383ccbc8f3f0d0b903f50a3912099.

--- main.js
+++ main.js
@@ -44,10 +44,8 @@ document.addEventListener('DOMContentLoaded', () => {
           return baseDate.toLocaleString([], {timeZone: 'UTC', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', fractionalSecondDigits: 2});
         } else if (baseDate.getUTCHours()) {
           return baseDate.toLocaleString([], {timeZone: 'UTC', hour: 'numeric', minute: 'numeric', second: 'numeric', fractionalSecondDigits: 2});
-        } else if (baseDate.getUTCMinutes()) {
-          return baseDate.toLocaleString([], {timeZone: 'UTC', minute: 'numeric', second: 'numeric', fractionalSecondDigits: 2});
         } else {
-          return baseDate.toLocaleString([], {timeZone: 'UTC', second: 'numeric', fractionalSecondDigits: 2});
+          return baseDate.toLocaleString([], {timeZone: 'UTC', minute: 'numeric', second: 'numeric', fractionalSecondDigits: 2});
         }
       })(new Date(Number(comment.vposMsec)))
     };
